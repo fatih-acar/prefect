93d7a062d4543eae1045a9dfedb2f3a6e48c9779
Fatih Acar <fatih@opsmill.com>
2025-04-30 10:11:12 +0200

    fix(triggers): workaround in-memory automations map
    We need to refresh the in-memory automations list when running
    distributed mode. A quick and dirty workaround is to reload all of them
    periodically.
    We also need to filter out garbage collected triggers while processing an
    event.
    
    Signed-off-by: Fatih Acar <fatih@opsmill.com>

--- src/prefect/server/events/triggers.py
+++ src/prefect/server/events/triggers.py
@@ -649,7 +649,7 @@ def _automations_lock() -> asyncio.Lock:
 
 def find_interested_triggers(event: ReceivedEvent) -> Collection[EventTrigger]:
     candidates = triggers.values()
-    return [trigger for trigger in candidates if trigger.covers(event)]
+    return [trigger for trigger in candidates if trigger.covers(event) and trigger._automation()]
 
 
 def load_automation(automation: Optional[Automation]) -> None:
@@ -1102,6 +1102,10 @@ async def proactive_evaluation(
 
 
 async def evaluate_proactive_triggers() -> None:
+    async with automations_session() as session:
+        async with _automations_lock():
+            await load_automations(session)
+
     for trigger in triggers.values():
         if trigger.posture != Posture.Proactive:
             continue
